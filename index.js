d0c3be456e0931eb6096272d714295466b72674a
kurui <kuruijoshua@gmail.com>
2025-06-17 00:35:56 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+// Function 1: writeCards
+function writeCards(names, event) {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    messages.push(message);
+  }
+  return messages;
+}
+
+// Function 2: countDown
+function countDown(startingNumber) {
+  while (startingNumber >= 0) {
+    console.log(startingNumber);
+    startingNumber--;
+  }
+}
